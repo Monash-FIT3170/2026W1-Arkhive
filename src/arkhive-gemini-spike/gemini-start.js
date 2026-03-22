070bae605e18ee9aca2273c43d2ee2c868aa1b5c
Aryan Cyrus <aryan.m10@yahoo.com>
2026-03-22 15:36:29 +1100

    feature: implemented simple spike for gemini API call

---
+++ src/arkhive-gemini-spike/gemini-start.js
@@ -0,0 +1,39 @@
+
+//============================================================
+//File:        gemini-start.js
+//Author:      Aryan Cyrus 33114242
+//Created:     2026-03/22
+//Description: Tech Spike for Google Gemini LLM
+//Version:     1.0
+//Last Updated:2026-03-22 by Aryan Cyrus
+//============================================================
+
+//imports
+import dotenv from 'dotenv'
+import { GoogleGenerativeAI } from '@google/generative-ai'
+
+//Configure .env file
+dotenv.config();
+
+//obtain API key from .env file
+API_KEY = process.env.API_KEY;
+//initialise Google generative AI object
+const genAI = new GoogleGenerativeAI(API_KEY)
+
+//Function to run generative AI model
+async function run() {
+    //Generate AI model using google's "gemeni-pro" model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" })
+
+    //provide a custom prompt for AI to respond to
+    const prompt = "Briefly explain the concept of OCR and describe its uses"
+    //feed prompts into gemeni-pro and get a result
+    const result = await model.generateContent(prompt);
+    //obtain actual response from result object
+    const response = await result.response;
+    //turn response into text
+    const text = response.text()
+    //print response
+    console.log(text)
+
+}
