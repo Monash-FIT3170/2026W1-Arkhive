1b9cf3f13087e0177e0c0cfe03314a573fc82da6
Harsha Sharma <hsha0051@student.monash.edu>
2026-04-22 09:11:01 +1000

    added the very basic backend structure for this project, so far only focusing on the ocr files and very basic server/client structure (a lot of other files are missing). If any changes are suggested, confirm it asap

---
+++ backend/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.use(express.json()) ;
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running locally at :${PORT}`);
+});
