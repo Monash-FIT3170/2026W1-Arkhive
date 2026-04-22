ff7a225307c86bb2f23f184a5baf001fe595b708
AryanPunekar007 <160367596+AryanPunekar007@users.noreply.github.com>
2026-04-22 14:49:18 +1000

    feat: [86d2bz0fn] Added image display component

--- frontend/src/pages/validation/components/document/DocumentPanel.tsx
+++ frontend/src/pages/validation/components/document/DocumentPanel.tsx
@@ -1,11 +1,18 @@
+import mockImage from "../../../../mock-data/test.png";
+
 function DocumentPanel() {
-	return (
-		<>
-			<div className="h-full w-full bg-base-200 border border-gray-200 rounded-lg">
-				<h1>Document PANEL</h1>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="h-full w-full bg-base-200 border border-gray-200 rounded-lg">
+        <h1>Document PANEL</h1>
+        <img
+          src={mockImage}
+          alt="Mock Image for Document Panel"
+          className="w-full h-auto object-cover rounded-lg"
+        />
+      </div>
+    </>
+  );
 }
 
 export default DocumentPanel;
