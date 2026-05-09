77d99002d18f7cfd2d169d79f418283299131300
msha0166 <msha0166@student.monash.edu>
2026-05-10 02:47:39 +1000

    Refactor routing in main.tsx to replace PreviewPage with ValidationPage and update index.css for full-width layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './index.css'
 import UploadPage from './pages/upload/UploadPage'
-import PreviewPage from './pages/preview/PreviewPage'
+import ValidationPage from './pages/validation/ValidationPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UploadPage />} />
-        <Route path="/preview" element={<PreviewPage />} />
+        <Route path="/validation" element={<ValidationPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
