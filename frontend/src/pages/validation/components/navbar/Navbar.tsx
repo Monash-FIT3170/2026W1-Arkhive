a882179f30215b1a6634da9a50aba5b464ce2621
Aryan Cyrus <aryan.m10@yahoo.com>
2026-05-22 22:11:39 +1000

    UI fix: updated navbar responsiveness

--- frontend/src/pages/validation/components/navbar/Navbar.tsx
+++ frontend/src/pages/validation/components/navbar/Navbar.tsx
@@ -1,6 +1,14 @@
-import { Sun, Moon, Upload, LayoutGrid, Columns2 } from 'lucide-react';
+import { Sun, Moon, Upload, LayoutGrid, Columns2, Share2Icon } from 'lucide-react';
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleStepClick = (route: string) => {
+    navigate(route);
+  };
+
+
   return (
     <div>
       <div className="navbar bg-base-200 text-base-content px-17 border-b border-base-300">
@@ -9,18 +17,29 @@ export const Navbar = () => {
         </div>
         <div className="ml-auto flex items-center gap-6">
           <ul className="steps">
-            <li className="step step-primary"><span className="step-icon"><Upload className="w-4 h-4" /></span>Upload</li>
-            <li className="step step-primary"><span className="step-icon"><LayoutGrid className="w-4 h-4" /></span>Document Preview</li>
-            <li className="step step-primary"><span className="step-icon"><Columns2 className="w-4 h-4" /></span>Validation</li>
-            <li className="step"><span className="step-icon"></span>Export</li>
-            <li>
-              <label className="swap swap-rotate cursor-pointer mx-2 top-1.5">
-                {/* hidden checkbox that toggles night/day theme */}
-                <input type="checkbox" value="night" className="theme-controller" />
-                <Sun className="swap-off w-8 h-8" />
-                <Moon className="swap-on w-8 h-8" />
-              </label>
+            <li className="step step-primary cursor-pointer " onClick={() => handleStepClick("/")}>
+              <span className="step-icon"><Upload className="w-4 h-4 hover:scale-130 transition" /></span>
+              Upload
             </li>
+            <li className="step step-primary cursor-pointer" onClick={() => handleStepClick("/preview")}>
+              <span className="step-icon"><LayoutGrid className="w-4 h-4 hover:scale-130 transition" /></span>
+              Document Preview
+            </li>
+            <li className="step step-primary cursor-pointer" onClick={() => handleStepClick("/validation")}>
+              <span className="step-icon"><Columns2 className="w-4 h-4 hover:scale-130 transition" /></span>
+              Validation
+            </li>
+            <li className="step animate-pulse cursor-pointer" onClick={() => handleStepClick("/export")}>
+              <span className="step-icon"><Share2Icon className="w-4 h-4 hover:scale-130 transition" /></span>
+              Export
+            </li>
+            <label className="swap swap-rotate cursor-pointer mx-2">
+              {/* hidden checkbox that toggles night/day theme */}
+              <input type="checkbox" value="night" className="theme-controller hover:scale-110 transition" />
+              <Sun className="swap-off w-8 h-8 hover:scale-110 transition" />
+              <Moon className="swap-on w-8 h-8 hover:scale-110 transition" />
+              <span className="sr-only">Toggle Theme</span>
+            </label>
           </ul>
         </div>
       </div>
