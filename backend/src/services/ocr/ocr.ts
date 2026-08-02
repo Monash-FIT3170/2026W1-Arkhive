56ff03620f973d38922f0758257f6358931f5ef9
cyr0006 <Aryan.m10@yahoo.com>
2026-08-02 19:46:48 +1000

    Deployment: Converts google vision credentials json into b64 for render deployment

--- backend/src/services/ocr/ocr.ts
+++ backend/src/services/ocr/ocr.ts
@@ -4,7 +4,19 @@ import fs from 'fs';
 import { extractStructuredComponents } from './utils/utils_table_extraction.js';
 import { withRetry } from './utils/utils.js';
 
-
+/**
+ * @author Aryan Cyrus (33114242)
+ * Initializes the Google Cloud Vision client with the necessary credentials.
+ * 
+ * We are when deploying one needs to add env variables to hosting platform BUT:
+ * - Render does not take JSON env variables, so we need to 
+ *   convert the JSON to base64 and then decode it back to JSON in the code.
+ * 
+ * So all render sees is: GOOGLE_CREDENTIALS_BASE64 = <base64 encoded JSON>
+ */
+const credsPath = path.join("/tmp", "google-vision-key.json");
+fs.writeFileSync(credsPath, Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64!, "base64"));
+process.env.GOOGLE_APPLICATION_CREDENTIALS = credsPath;
 
 const client = new vision.ImageAnnotatorClient({
   keyFilename: path.resolve(
@@ -49,14 +61,14 @@ export async function textExtraction(buffer: Buffer): Promise<string> {
 
 function for getting bounding boxes for all words detected
  @author Harsha Sharma (33879303)
-*/  
+*/
 async function parseTable(imageBuffer: Buffer) {
   const [response] = await client.documentTextDetection(imageBuffer);
   const fullTextAnnotation = response.fullTextAnnotation;
   return extractStructuredComponents(fullTextAnnotation!.pages!);
 }
 
-export async function parseTableWithRetries(imageBuffer: Buffer){
+export async function parseTableWithRetries(imageBuffer: Buffer) {
   return await withRetry(() => parseTable(imageBuffer))
 }
 
