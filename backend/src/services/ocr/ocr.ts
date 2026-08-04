a35b3e0d1c8f2967e2d13dae14e06f4d98962f36
cyr0006 <Aryan.m10@yahoo.com>
2026-08-04 12:04:05 +1000

    google vision api is obtained now from ../temp/google-vision-key.json

--- backend/src/services/ocr/ocr.ts
+++ backend/src/services/ocr/ocr.ts
@@ -21,7 +21,7 @@ process.env.GOOGLE_APPLICATION_CREDENTIALS = credsPath;
 const client = new vision.ImageAnnotatorClient({
   keyFilename: path.resolve(
     process.cwd(),
-    "../backend/src/credentials/google-vision-key.json"
+    "../temp/google-vision-key.json"
   ),
   features: [
     {
