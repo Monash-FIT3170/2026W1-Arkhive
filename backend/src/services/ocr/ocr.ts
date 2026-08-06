559c8e52d2aeada779599215b77ce17cfe7f0159
Harsha Sharma <hsha0051@student.monash.edu>
2026-08-07 00:41:49 +1000

    refactor: textExtraction is refactored to use LlamaCloud instead of Google Cloud Vision. Will test in later commits to see if it is stable removed API key

--- backend/src/services/ocr/ocr.ts
+++ backend/src/services/ocr/ocr.ts
@@ -1,15 +1,37 @@
 import path from 'path';
 import vision from '@google-cloud/vision';
-import fs from 'fs';
+import fs, { ReadStream } from 'fs';
 import { extractStructuredComponents } from './utils/utils_table_extraction.js';
 import { withRetry } from './utils/utils.js';
+import LlamaCloud from "@llamaindex/llama-cloud";
+import { file } from 'zod';
+import { text } from 'stream/consumers';
+import { Readable } from 'stream';
+import { Uploadable } from "@llamaindex/llama-cloud"; 
 
+const client2 = new LlamaCloud({
+  apiKey: process.env.LLAMA_API_KEY,
+});
 
+const fileObj = await client2.files.create({
+  file: fs.createReadStream("./sample-file-1_page-0001.jpg"),
+  purpose: "extract",
+});
+
+/*const result = await client2.extract.create({
+  file_input: fileObj.id,
+  configuration: {
+      data_schema: dataSchemaExtract,
+      extraction_target: 'per_doc',
+      tier: "cost_effective",
+      confidence_scores: true,
+  },
+});*/
 
 const client = new vision.ImageAnnotatorClient({
   keyFilename: path.resolve(
     process.cwd(),
-    "../backend/src/credentials/google-vision-key.json"
+    "../../credentials/google-vision-key.json"
   ),
   features: [
     {
@@ -21,17 +43,27 @@ const client = new vision.ImageAnnotatorClient({
   }
 });
 
-const chunk = <T>(arr: T[], size: number): T[][] =>
-  Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
-    arr.slice(i * size, i * size + size)
-  );
-
 export async function textExtraction(buffer: Buffer): Promise<string> {
-  const [result] = await client.documentTextDetection({
+  /*const [result] = await client.documentTextDetection({
     image: { content: buffer.toString("base64") }
-  });
+  });*/
+  const readableStream = fs.createReadStream(buffer)
 
-  return result.fullTextAnnotation?.text ?? "";
+  const fileObj = await client2.files.create({
+  file: readableStream,
+  purpose: "extract",
+});
+
+  const result = await client2.parsing.parse({
+  file_id: fileObj.id,
+  tier: "fast",
+  expand: ["markdown_full", "metadata"],
+  version: "latest",
+  output_options: {
+    granular_bboxes: ["cell"]
+  }})
+
+  return result.markdown_full ?? "";
 }
 
 // test ocr on 1 png page
@@ -63,9 +95,11 @@ export async function parseTableWithRetries(imageBuffer: Buffer){
 // function for getting overall averaged confidence score
 
 const jsonOut = JSON.stringify(
-  await parseTable(fs.readFileSync("assets/sample-page-1.png")),
+  await parseTable(fs.readFileSync("./receipt-template-us-classic-white-750px.png")),
   null,
   2
 );
 
 fs.writeFileSync("boundingBox.json", jsonOut, "utf-8");
+
+console.log(await textExtraction(fs.readFileSync("./receipt-template-us-classic-white-750px.png")))
