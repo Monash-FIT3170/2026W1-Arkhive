fec98b0d14947835cd128d925e7ffa3b7153b956
cyr0006 <Aryan.m10@yahoo.com>
2026-08-30 15:57:17 +1000

    Frontend intergration; wired into CI yaml

--- frontend/vitest.integration.config.ts
+++ frontend/vitest.integration.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vitest/config";
-import { TEST_BACKEND_PORT } from "./src/integration/constants";
+import { TEST_BACKEND_PORT } from "./src/integration/constants.ts";
 
 export default defineConfig({
   test: {
-    environment: "jsdom",
+    environment: "node",
     globals: true,
     include: ["**/*.integration.test.ts"],
     env: {
       VITE_API_URL: `http://localhost:${TEST_BACKEND_PORT}`,
     },
+    globalSetup: ["./src/integration/globalSetup.ts"],
+    setupFiles: ["./src/integration/setupFetchEnvironment.ts"],
   },
 });
