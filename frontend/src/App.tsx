6840b8cbbad0d863fdf01cd9703c0c7c96f6d559
Simon <skatsi07@icloud.com>
2026-09-09 09:29:34 +1000

    feat [14yk43r4pux]: implement user authentication and protect routes with AuthGuard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,32 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
+import { AuthGuard } from "./components/auth/AuthGuard";
+import LoginPage from "./pages/login/LoginPage";
 import Navbar from "./pages/validation/components/navbar/Navbar";
 import UploadPage from "./pages/upload/UploadPage";
 import ValidationPage from "./pages/validation/ValidationPage";
 
 function App() {
 	return (
-		<Router>
-			<div className="min-h-screen bg-base-100 text-base-content flex flex-col">
-				<Navbar />
-				<div className="flex-1 flex flex-col">
-					<Routes>
-						<Route path="/" element={<UploadPage />} />
-						<Route path="/validation" element={<ValidationPage />} />
-					</Routes>
+		<AuthProvider>
+			<Router>
+				<div className="min-h-screen bg-base-100 text-base-content flex flex-col">
+					<Navbar />
+					<div className="flex-1 flex flex-col">
+						<Routes>
+							<Route path="/login" element={<LoginPage />} />
+							<Route element={<AuthGuard />}>
+								<Route path="/" element={<UploadPage />} />
+								<Route path="/validation" element={<ValidationPage />} />
+							</Route>
+						</Routes>
+					</div>
 				</div>
-			</div>
-		</Router>
+			</Router>
+		</AuthProvider>
 	);
 }
 
 export default App;
+
